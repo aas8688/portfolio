8c45f9541c37032f3ce5e5b8830cd87be2e4bb52
Jason <Jason@Jasons-MacBook-Pro.local>
2017-05-26 13:27:31 -0400

    add content

--- app/js/main.js
+++ app/js/main.js
@@ -1,18 +1,18 @@
-var navProductOne = document.getElementById('product-one'),
-    navProductTwo = document.getElementById('product-two'),
-    navProductThree = document.getElementById('product-three'),
-    navProductFour = document.getElementById('product-four'),
-    navProductFive = document.getElementById('product-five'),
-    navProductSix = document.getElementById('product-six'),
-    navProductSeven = document.getElementById('product-seven');
+var navProductOne = document.getElementById('product-one');
+var navProductTwo = document.getElementById('product-two');
+var navProductThree = document.getElementById('product-three');
+var navProductFour = document.getElementById('product-four');
+var navProductFive = document.getElementById('product-five');
+var navProductSix = document.getElementById('product-six');
+var navProductSeven = document.getElementById('product-seven');
 
-var productContentOne = document.getElementById('product-content-one'),
-    productContentTwo = document.getElementById('product-content-two'),
-    productContentThree = document.getElementById('product-content-three'),
-    productContentFour = document.getElementById('product-content-four'),
-    productContentFive = document.getElementById('product-content-five'),
-    productContentSix = document.getElementById('product-content-six'),
-    productContentSeven = document.getElementById('product-content-seven');
+var productContentOne = document.getElementById('product-content-one');
+var productContentTwo = document.getElementById('product-content-two');
+var productContentThree = document.getElementById('product-content-three');
+var productContentFour = document.getElementById('product-content-four');
+var productContentFive = document.getElementById('product-content-five');
+var productContentSix = document.getElementById('product-content-six');
+var productContentSeven = document.getElementById('product-content-seven');
 
 
 var navAbout = document.getElementById('about');
@@ -34,8 +34,15 @@ function hideAllProductContent() {
     productContentFive.style.display = "none";
     productContentSix.style.display = "none";
     productContentSeven.style.display = "none";
-
 }
+
+
+window.onload = function() {
+
+
+};
+
+
 
 function removeClassActive() {
     navProductOne.className = "";
@@ -69,9 +76,9 @@ function displayProduct() {
 
 btnClose.onclick = function() {
 
-    removeClassActive();
-    hideAllProductContent();
-    productContainer.style.bottom = "-100%";
+      removeClassActive();
+      hideAllProductContent();
+      productContainer.style.bottom = "-100%";
 
 };
 
